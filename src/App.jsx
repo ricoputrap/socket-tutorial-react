e07b15511f0bf81a5f26605594112fb84a13c0ea
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-09-10 23:15:55 +0700

    Setup client connection to the socket io server

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001", {
+  transports: ["websocket"]
+})
+
+socket.on("connect_error", (err) => {
+  console.log("===== ERROR:", err)
+})
 
 function App() {
 
+  const sendMessage = () => {
+    
+  }
+
   return (
-    <div>
-      HAHA
-    </div>
+    <main>
+      <input type="text" placeholder='Message...'/>
+      <button onClick={sendMessage}>Send Message</button>
+    </main>
   )
 }
 
